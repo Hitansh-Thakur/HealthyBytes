8f47f15e42f72853cddc91fbb914bac6c803855e
Hitansh Thakur <thakurhitansh4325@gmail.com>
2024-02-26 20:29:06 +0530

    made components salad_card & Quantity btn
    made qty btn & salad card a component and stored them in the components folder.
    then we can include them wherever needed.

---
+++ components/quantity_component.php
@@ -0,0 +1,44 @@
+<div class="qty-container d-flex align-items-center">
+    <input class="btn btn-primary mr-3" type="submit" value="Add to Cart">
+    <button class="qty-btn-minus btn btn-primary rounded-left" type="button"><i class="fa fa-minus"></i></button>
+    <input type="text" name="qty" value="1" class="input-qty form-control border-0 rounded-0 text-center"
+        style="width:60px" />
+    <button class="qty-btn-plus btn btn-primary rounded-right" type="button"><i class="fa fa-plus"></i></button>
+</div>
+
+<script>
+    var incrementButton = document.querySelector('.qty-btn-plus');
+    var decrementButton = document.querySelector('.qty-btn-minus');
+    var inputField = document.querySelector('.input-qty');
+
+    incrementButton.addEventListener('click', function () {
+        inputField.value = parseInt(inputField.value) + 1;
+    });
+
+    decrementButton.addEventListener('click', function () {
+        var quantity = parseInt(inputField.value);
+        if (quantity > 1) {
+            inputField.value = quantity - 1;
+        }
+    });
+
+    inputField.addEventListener('keypress', function (e) {
+        if (e.key === '-' || e.key === '+' || e.key === 'e' || e.key === '.') {
+            e.preventDefault();
+        }
+    });
+
+    inputField.addEventListener('paste', function (e) {
+        e.preventDefault();
+        var pasteData = e.clipboardData.getData('text/plain');
+        if (!isNaN(pasteData)) {
+            inputField.value = parseInt(pasteData);
+        }
+    });
+
+    inputField.addEventListener('change', function () {
+        if (isNaN(inputField.value)) {
+            inputField.value = 1;
+        }
+    });
+</script>
---
+++ components/salad_card.php
@@ -0,0 +1,20 @@
+<?php foreach ($products as $salad): ?>
+    <?php if ($salad['category'] == $category): ?>
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-3">
+            <div class="card" style="max-width: 18rem;">
+                <img class="card-img-top" src="./uploads/<?php echo $salad['salad_img']; ?>" alt="Card image cap" height="200px" style="object-fit:cover;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $salad['salad_name']; ?></h5>
+                    <p class="card-text text-truncate text-dark d-block" style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;"><?php echo $salad['salad_desc']; ?></p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><?php echo $salad['category']; ?></li>
+                </ul>
+                <div class="card-body">
+                    <span class="text-dark font-weight-bold align-middle"><?php echo $salad['salad_price']; ?> ₹ </span>
+                    <a href="product_details.php?id=<?php echo $salad['id']; ?>" class="btn btn-primary float-right align-middle">View</a>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+<?php endforeach; ?>
