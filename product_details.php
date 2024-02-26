1e35f8f56746abede4cfdf7729977ee512393fe5
Hitansh Thakur <thakurhitansh4325@gmail.com>
2024-02-26 20:19:39 +0530

    Added Special cooking instructions & Solved Quantity btn
    Quantity btn is fixed and will not allow negative input or floating point no.

--- product_details.php
+++ product_details.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Product Description Page</title>
-  <link rel="stylesheet" href="product_details.css">
-  <link rel="stylesheet" href="index.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
 <?php
 session_start();
-include_once 'nav.php';
 include_once 'db_connect.php';
 $id = $_GET['id'];
 $result = $conn->query("SELECT * FROM salads WHERE id = $id");
@@ -20,26 +7,39 @@ $row = $result->fetch_assoc();
 $name = $row['salad_name'];
 $desc = $row['salad_desc'];
 $price = $row['salad_price'];
-$nutritional_content = $row['nutritional_content'];
+$category = $row['category'];
 $ingredients = $row['ingredients'];
 $img = $row['salad_img'];
 $result->free();
-$conn->close();
-
-
-
-
-
-
+// $conn->close();
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $name ?>| Healthy Bytes</title>
+  <link rel="stylesheet" href="product_details.css">
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+
 <body>
-  <div class="container-fluid pt-4" style="width:95vw">
+  <?php include 'nav.php'; ?>
+  <div class="main container-fluid pt-4" style="width:95vw">
     <div class="row">
       <div class="col-md-6">
-        <img src="<?php echo "./uploads/" . $img ?>" class="img-fluid product-image" alt="Product Image" style="height:75vh;object-fit:cover">
+        <img src="<?php echo "./uploads/" . $img ?>" class="img-fluid product-image" alt="Product Image"
+          style="height:75vh;object-fit:cover">
       </div>
-      <div class="col-md-6 text-dark">
+      <div class="col-md-6 text-dark text-justify">
         <h1 class="text-dark">
           <?php echo $name ?>
         </h1>
@@ -49,7 +49,7 @@ $conn->close();
         <p><b>Ingredients : </b>
           <?php echo $ingredients ?>
         </p>
-        <table class="table" border="2dp">
+        <table class="table table-condensed" border="2dp">
           <th>Nutritional Information (Serving Size : 300g)</th>
           <tr>
             <td>Carbohydrates</td>
@@ -74,75 +74,30 @@ $conn->close();
         </p>
         <form action="cart.php" method="post">
 
-        <!-- <a href="cart.php" class="btn btn-primary">Add to Cart</a> -->
-        <!-- add quantity of the product use bootstrap-->
-        <!-- TODO : add quantity of the product use bootstrap -->
-          <div class="qty-container d-flex align-items-center  ">
-            <input class="btn btn-primary mr-3" type="submit" value="Add to Cart">
-            <button class="qty-btn-minus btn btn-primary rounded-left" type="button"><i class="fa fa-minus"></i></button>
-            <input type="text" name="qty" value="1" class="input-qty form-control border-0 rounded-0 text-center " style="width:60px" />
-            <button class="qty-btn-plus btn btn-primary rounded-right" type="button"><i class="fa fa-plus"></i></button>
+          <div class="form-group">
+            <label for="special_instructions">Special Cooking Instructions:</label>
+            <textarea placeholder="Eg. Add more vegetables
+       Add more nuts
+            " class="form-control" id="special_instructions" name="special_instructions" rows="3" style="resize:none;"></textarea>
           </div>
 
-        <?php
-        // take quantity of the product from        
-        // $quantity = 
+          <!-- Quantity Conponent -->
+          <?php include 'components/quantity_component.php'; ?>
 
-        echo '<input type="hidden" name="product_id" value="' . $id . '">';
-        echo '<input type="hidden" name="product_name" value="' . $name . '">';
-        echo '<input type="hidden" name="product_price" value="' . $price . '">';
-        echo '<input type="hidden" name="product_image" value="uploads/' . $img . '">';
-        echo '<input type="hidden" name="add_to_cart" value="1">';
-        ?>
-        
-        
-        
-        
-      </form>
+          <?php
 
-
+          echo '<input type="hidden" name="product_id" value="' . $id . '">';
+          echo '<input type="hidden" name="product_name" value="' . $name . '">';
+          echo '<input type="hidden" name="product_price" value="' . $price . '">';
+          echo '<input type="hidden" name="product_image" value="uploads/' . $img . '">';
+          echo '<input type="hidden" name="add_to_cart" value="1">';
+          ?>
+        </form>
       </div>
     </div>
   </div>
 
- <script>
-  // Quantity increment and decrement in js
-  var incrementButton = document.querySelector('.qty-btn-plus');
-  var decrementButton = document.querySelector('.qty-btn-minus');
-  var inputField = document.querySelector('.input-qty');
 
-  incrementButton.addEventListener('click', function () {
-    inputField.value = parseInt(inputField.value) + 1;
-  });
-
-  decrementButton.addEventListener('click', function () {
-    inputField.value = parseInt(inputField.value) - 1;
-  });
-
-  // Prevent negative quantity
-  inputField.addEventListener('change', function () {
-    if (parseint(inputField.value) < 0) {
-      inputField.value = 0;
-    }
-  });
-
-  // Prevent non-numeric input
-  inputField.addEventListener('keypress', function (e) {
-    if (e.key === '-' || e.key === '+' || e.key === 'e') {
-      e.preventDefault();
-    }
-  });
-
-  // Prevent decimal input
-  inputField.addEventListener('input', function () {
-    if (this.value.indexOf('.') !== -1) {
-      this.value = this.value.slice(0, this.value.indexOf('.'));
-    }
-  });
-
-  // Prevent non-numeric input
-
- </script>
   <?php include 'footer.php'; ?>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
